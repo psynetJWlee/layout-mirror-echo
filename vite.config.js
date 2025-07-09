5befc48f7db27351245e3be3dedeb8779f7e3cba
psynetJWlee <probe35331@gmail.com>
2025-07-09 09:15:03 +0900

    package-locke생성

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-
 export default defineConfig({
-  root: '.',
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 5173
-  }
-})
+    root: '.',
+    build: {
+      outDir: 'dist'
+    },
+    server: {
+      port: 8080  
+    }
+  })
+  
